06be98b83edd4609c4a57d589c17a0f6e7d9ea33
Kahlolo <kahlolotlanya11@gmail.com>
2025-11-03 21:13:57 +0200

    changes on servewr js

--- server.js
+++ server.js
@@ -8,25 +8,25 @@ import { pool } from "./db.js";
 
 dotenv.config();
 
+const __filename = fileURLToPath(
+    import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/students", studentsRouter);
 
-const __filename = fileURLToPath(
-    import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // Serve React build
-app.use(express.static(path.join(__dirname, "mapp/dist")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 // Catch-all route for React SPA
 app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "mapp/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 async function startServer() {
     try {
