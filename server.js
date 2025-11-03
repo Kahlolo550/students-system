ef5e4a750097b7cd80425861ebd9027239827d3f
Kahlolo <kahlolotlanya11@gmail.com>
2025-11-03 16:46:32 +0200

    third committtt

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import studentsRouter from "./routes/students.js";
+import { pool } from "./db.js";
 
 dotenv.config();
 
@@ -12,6 +13,20 @@ app.use(express.json());
 app.use("/students", studentsRouter);
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(` Server running on http://localhost:${PORT}`);
-});
+
+async function startServer() {
+    try {
+        const connection = await pool.getConnection();
+        console.log("Database connected successfully");
+        connection.release();
+
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+        });
+    } catch (err) {
+        console.error("Failed to connect to the database:", err);
+        process.exit(1);
+    }
+}
+
+startServer();
